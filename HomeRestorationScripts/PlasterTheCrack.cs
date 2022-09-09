e330749ffa50f28287acabc79ee6f33c668c328d
fatihtkn <fatihh5455@gmail.com>
2022-09-09 17:00:50 +0300

    modified

--- HomeRestorationScripts/PlasterTheCrack.cs
+++ HomeRestorationScripts/PlasterTheCrack.cs
@@ -2,167 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlasterTheCrack : MonoBehaviour
+public class PlasterTheCrack : MonoBehaviour,IOnPainted
 {
-    
-    public Texture2D dirtBrush;
-    public Texture2D cloneTextureBase;
-    public Texture2D transparentTex;
-                     private Texture2D cloneTexture;
-    [SerializeField] private LayerMask mask;
     public GameObject trowel;
-    public static Color currentColor;
-    public static PlasterTheCrack plaster_sc;
-    [SerializeField]private Material mat;
-    private bool control;
-    public Color test;
+    public static PlasterTheCrack Instance;
+    private ITexturePainter texturePainter;
 
-    private float dirtAmountTotal, dirtAmount, removedDirtAmount;
-
-   
-    private void Start()
+    private void Awake()
     {
         
-        control = true;
-        
-        
-        plaster_sc = GetComponent<PlasterTheCrack>();
+        texturePainter = GetComponent<ITexturePainter>();
 
-        ClearMainTexture();
-
-        SetCloneTexPixels();
-
-        CalculateDirtAmount();
-        
+    }
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = GetComponent<PlasterTheCrack>();
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     private void Update()
     {
-        PaintTexture();
+        bool controller = GameManager.state == GameManager.GameStates.Plastering & Input.GetMouseButton(0);
 
-        if (Mathf.RoundToInt(DirtAmountFunc() * 100) >= 89.5f & GameManager.state == GameManager.GameStates.Plastering&control)
+        if (controller)
         {
-
-
-            ConfettiManager.confettiSc.CreateConfetti();
-            ButtonFunctionsController.buttonManager.setToWallPaintingButton.SetActive(true);
-            control = false;
-            
+            texturePainter.PaintTexture(trowel, new Vector3(2f, 0, 0));
         }
 
     }
 
-    public float DirtAmountFunc()
+
+    public void OnPainted() //Interface Implementation
     {
-
-        return removedDirtAmount / dirtAmountTotal;
-    }
-    public void SetCloneTexPixels()
-    {
-        cloneTexture = new Texture2D(cloneTextureBase.width, cloneTextureBase.height);
-        cloneTexture.SetPixels(cloneTextureBase.GetPixels());
-        cloneTexture.Apply();
-    }
-
-    private void ClearMainTexture()
-    {
-        if (transparentTex != null)
-        {
-
-            for (int i = 0; i < transparentTex.width; i++)
-            {
-                for (int j = 0; j < transparentTex.height; j++)
-                {
-                    transparentTex.SetPixel(i, j, Color.clear);
-                }
-            }
-            transparentTex.Apply();
-        }
-            
-
-       
-    }
-    private void CalculateDirtAmount()
-    {
-        for (int x = 0; x < cloneTextureBase.width; x++)
-        {
-            for (int y = 0; y < cloneTextureBase.height; y++)
-            {
-                dirtAmountTotal += cloneTextureBase.GetPixel(x, y).g;
-            }
-        }
-        dirtAmount = dirtAmountTotal;
-        removedDirtAmount = 0f;
-    }
-    private void PaintTexture()
-    {
-        currentColor = ColorManagerSc.color_Manager.currentColor;
-        if (GameManager.state == GameManager.GameStates.Plastering)
-        { 
-
-
-            if (Input.GetMouseButton(0))
-            {
-
-                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit,mask))
-                {
-                    
-                    trowel.transform.position = raycastHit.point+new Vector3(2,0,0);
-                    Vector2 textureCoord = raycastHit.textureCoord;
-                    Renderer rend = raycastHit.collider.GetComponent<MeshRenderer>();
-                    Texture2D tex = rend.material.mainTexture as Texture2D;
-
-                    int pixelX = (int)(textureCoord.x * tex.width);
-                    int pixelY = (int)(textureCoord.y * tex.height);
-
-                    if (textureCoord == null)
-                    {
-                        return;
-                    }
-
-
-
-
-                    //DirtBrush
-                    int pixelXOffset = pixelX - (dirtBrush.width / 2);
-                    int pixelYOffset = pixelY - (dirtBrush.height / 2);
-
-
-                    for (int x = 0; x < dirtBrush.width; x++)
-                    {
-                        for (int y = 0; y < dirtBrush.height; y++)
-                        {
-                            Color pixelDirt = dirtBrush.GetPixel(x, y);
-                            Color pixelDirtMask = cloneTexture.GetPixel(pixelXOffset + x, pixelYOffset + y);
-
-                            float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
-                            removedDirtAmount += removedAmount;
-
-                            cloneTexture.SetPixel(
-                                pixelXOffset + x,
-                                pixelYOffset + y,
-                                new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
-
-
-
-                            tex.SetPixel(pixelXOffset + x,
-                                pixelYOffset + y, currentColor);
-                            
-                            
-
-                        }
-
-                    }
-
-                    ProgressBar.progressbarSc.SetBar(DirtAmountFunc());
-                    tex.Apply();
-                    cloneTexture.Apply();
-
-
-                }
-            }
-
-        }
-
+        ConfettiManager.confettiSc.CreateConfetti();
+        ButtonFunctionsController.buttonManager.setToWallPaintingButton.SetActive(true);
     }
 }
--- HomeRestorationScripts/WallPainting.cs
+++ HomeRestorationScripts/WallPainting.cs
@@ -2,150 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallPainting : MonoBehaviour
+public class WallPainting : MonoBehaviour, IOnPainted
 {
-    [SerializeField] private Texture2D wallbaseTexture;
-    [SerializeField] private LayerMask mask;
-    [SerializeField] private Texture2D dirtBrush;
-    [SerializeField] private Texture2D mainTexture;
-    [SerializeField] private Texture2D mainTextureReference;
-    [SerializeField] private Texture2D CrackBaseTex;
-
-    private bool control;
-    public Vector3 paintbrushOffset;
-    public bool startPaint;
-    public static WallPainting walpaint_sc;
-    private Texture2D wallCloneTex;
-    private Texture2D totalPixelCount;
-    private float dirtAmount, removedDirtAmount,dirtAmountTotal;
-    public Color color;
+    
+    public static WallPainting Instance;
+    private ITexturePainter texturePaint;
     
 
     private void Awake()
     {
-        walpaint_sc = GetComponent<WallPainting>();
-        //color = ColorManagerSc.color_Manager.currentColor;
-        control = true;
-    }
-    private void Start()
-    {
         
-
-        mainTexture.SetPixels(mainTextureReference.GetPixels());
-        mainTexture.Apply();
-
-        totalPixelCount = new Texture2D((wallbaseTexture.width + CrackBaseTex.width)/2, (wallbaseTexture.height + CrackBaseTex.height)/2);
-
-
-        wallCloneTex = new Texture2D(totalPixelCount.width, totalPixelCount.height);
-        wallCloneTex.SetPixels(wallbaseTexture.GetPixels());
-        wallCloneTex.Apply();
-
-        for (int x = 0; x < totalPixelCount.width; x++)
+        if (Instance == null)
         {
-            for (int y = 0; y < totalPixelCount.height; y++)
-            {
-                dirtAmountTotal += totalPixelCount.GetPixel(x, y).g;
-            }
+            Instance = GetComponent<WallPainting>();
         }
-        dirtAmount = dirtAmountTotal;
-        removedDirtAmount = 0f;
+        else
+        {
+            Destroy(this);
+        }
+        texturePaint = GetComponent<ITexturePainter>();
+        
     }
-
-
     private void Update()
     {
-        if (Mathf.RoundToInt(DirtAmountFunc() * 100) >= 93f&GameManager.state==GameManager.GameStates.WallPainting&control)
-        {
-
-            
-            ConfettiManager.confettiSc.CreateConfetti();
-            StartCoroutine(Cooldwon());
-            
-            control = false;
-            
-
-        }
+        
         if(GameManager.state == GameManager.GameStates.WallPainting & Input.GetMouseButton(0))
         {
-            
-             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit ray, mask))
-             {
-                ToolManager.toolmanagerSc.paintbrush.transform.position = ray.point+paintbrushOffset;
-                
-                 Vector2 textureCoord = ray.textureCoord;
-                 Renderer rend = ray.collider.GetComponent<Renderer>();
-                 Texture2D tex = rend.material.mainTexture as Texture2D;
 
-                 int pixelX = (int)(textureCoord.x * tex.width);
-                 int pixelY = (int)(textureCoord.y * tex.height);
-
-                 if (textureCoord == null)
-                 {
-                     return;
-                 }
-
-
-                 int pixelXOffset = pixelX - (dirtBrush.width / 2);
-                 int pixelYOffset = pixelY - (dirtBrush.height / 2);
-
-
-                 for (int x = 0; x < dirtBrush.width; x++)
-                 {
-                     for (int y = 0; y < dirtBrush.height; y++)
-                     {
-
-                         Color pixelDirt = dirtBrush.GetPixel(x, y);
-                         Color pixelDirtMask = wallCloneTex.GetPixel(pixelXOffset + x, pixelYOffset + y);
-
-                         float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
-                         dirtAmount -= removedAmount;
-                         removedDirtAmount += removedAmount;
-
-                         wallCloneTex.SetPixel(
-                             pixelXOffset + x,
-                             pixelYOffset + y,
-                             new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
-
-
-
-                         tex.SetPixel(pixelXOffset + x,
-                             pixelYOffset + y,ColorManagerSc.color_Manager.currentColor );
-
-
-                     }  
-
-                 }
-                 tex.Apply();
-                wallCloneTex.Apply();
-                 ProgressBar.progressbarSc.SetBar(DirtAmountFunc());
-             }
-            
-
+            texturePaint.PaintTexture(ToolManager.toolmanagerSc.paintbrush,new Vector3(1f,-2.3f,-0.7f));
         }
-        
-        
 
     }
-
-    public float DirtAmountFunc()
-    {
-
-        return removedDirtAmount / dirtAmountTotal;
-    }
-    private void PaintAllPixels()
-    {
-        for (int i = 0; i < mainTexture.width; i++)
-        {
-            for (int j = 0; j < mainTexture.height; j++)
-            {
-               mainTexture.SetPixel(i, j, color);
-            }
-        }
-    }
-    IEnumerator Cooldwon()
+    
+    IEnumerator Timer()
     {
         yield return new WaitForSeconds(2f);
         ButtonFunctionsController.buttonManager.restartButton.SetActive(true);
+    }
+
+    public void OnPainted()//Interface Implementation
+    {
+        ConfettiManager.confettiSc.CreateConfetti();
+        StartCoroutine(Timer());
     }
 }
